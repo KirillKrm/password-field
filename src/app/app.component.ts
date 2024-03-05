f225241fc53ec90d93e17d1fba0f18eeb58345df
KirillKrm <kirillkarmazin2301@gmail.com>
2024-03-05 15:20:58 +0200

    feat: added password input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'password-field';
+  password: string = '';
+  passwordStrength: string = '';
+
+  checkPasswordStrength() {
+    if (this.password.length === 0) {
+      this.passwordStrength = 'empty';
+      return;
+    }
+
+    const hasLetter = /[a-zA-Z]/.test(this.password);
+    const hasNumber = /[0-9]/.test(this.password);
+    const hasSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(
+      this.password
+    );
+
+    if (this.password.length < 8) {
+      this.passwordStrength = 'short';
+    } else if (hasLetter && hasNumber && hasSymbol) {
+      this.passwordStrength = 'strong';
+    } else if (
+      (hasLetter && hasSymbol) ||
+      (hasLetter && hasNumber) ||
+      (hasNumber && hasSymbol)
+    ) {
+      this.passwordStrength = 'medium';
+    } else {
+      this.passwordStrength = 'easy';
+    }
+  }
 }
